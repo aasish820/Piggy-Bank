99f46a3f40fcb35260b749d3f6aeffc9682a4634
Aasish <katwalaasish5@gmail.com>
2024-08-19 11:56:37 -0500

    test

---
+++ PiggyBank-Security/src/main/java/org/piggybank/security/entity/User.java
@@ -0,0 +1,9 @@
+package org.piggybank.security.entity;
+
+public class User {
+	private long id;
+	private String first_name;
+	private String last_name;
+	private String email;
+	private String password;
+}
